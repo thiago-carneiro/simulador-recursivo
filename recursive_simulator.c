ec26e677df253ae17b94f4e6f90d3eef8102687a
Thiago Carneiro <72167913+thiago-carneiro@users.noreply.github.com>
2023-01-03 18:54:55 -0300

    It works!

--- recursive_simulator.c
+++ recursive_simulator.c
@@ -135,7 +135,7 @@ geological_model_2d new_senoidal_model(unsigned int nx, unsigned int nz,
     {
         float depth = ((rand() / (float)RAND_MAX) * (max_depth - min_depth)) + min_depth;
         float amplitude = (rand() / (float)RAND_MAX) * max_depth;
-        float k = M_PI / ((rand() / (float)RAND_MAX) * 50);
+        float k = M_PI / ((rand() / (float)RAND_MAX) * 900 + 100);
         float phase = 2 * M_PI * (rand() / (float)RAND_MAX);
         float layer_speed = ((rand() / (float)RAND_MAX) * (max_speed - min_speed)) + min_speed;
         float layer_rho = ((rand() / (float)RAND_MAX) * (max_rho - min_rho)) + min_rho;
@@ -198,6 +198,133 @@ geological_model_2d new_senoidal_model(unsigned int nx, unsigned int nz,
     return model;
 }
 
+geological_model_2d new_parallel_model(unsigned int nx, unsigned int n_layers,
+                                       float *depths, float *speeds, float *rhos)
+{
+    if (!n_layers)
+    {
+        n_layers = 4;
+    }
+    float depths_v[] = {1000., 1000., 1000., 1000.};
+    if (!depths)
+    {
+
+        depths = depths_v;
+    }
+    float speeds_v[] = {1500., 2800., 4500., 3200.};
+    if (!speeds)
+    {
+        speeds = speeds_v;
+    }
+    float rhos_v[] = {1., 2., 2.16, 2.6};
+    if (!rhos)
+    {
+        rhos = rhos_v;
+    }
+    geological_model_1d *columns = (geological_model_1d *)malloc(nx * sizeof(geological_model_1d));
+    for (unsigned int x = 0; x < nx; x++)
+    {
+        layer *layers = (layer *)malloc(n_layers * sizeof(layer));
+        for (unsigned int l = 0; l < n_layers; l++)
+            init_layer(layers + l, depths[l], speeds[l], rhos[l]);
+        init_geological_model_1d(columns + x, n_layers, layers);
+    }
+    geological_model_2d model;
+    init_geological_model_2d(&model, nx, columns);
+    return model;
+}
+
+geological_model_2d new_dome_model(unsigned int nx, float *speeds, float *rhos)
+{
+    float speeds_v[] = {1500., 2800., 4500., 3200.};
+    if (!speeds)
+    {
+        speeds = speeds_v;
+    }
+    float rhos_v[] = {1., 2., 2.16, 2.6};
+    if (!rhos)
+    {
+        rhos = rhos_v;
+    }
+    geological_model_1d *columns = (geological_model_1d *)malloc(nx * sizeof(geological_model_1d));
+    float ray = nx / 6.;
+    for (unsigned int x = 0; x < nx; x++)
+    {
+        float cathetus_x = fabs(x - nx / 2.);
+        float cathetus_z;
+        unsigned int include_dome;
+        if (cathetus_x < ray)
+        {
+            cathetus_z = 7. * sqrt(ray * ray - cathetus_x * cathetus_x);
+            include_dome = 1;
+        }
+        else
+        {
+            cathetus_z = 0;
+            include_dome = 0;
+        }
+        layer *layers = (layer *)malloc((3 + include_dome) * sizeof(layer));
+        init_layer(layers, 1000, speeds[0], rhos[0]);
+        init_layer(layers + 1, 1000 - cathetus_z, speeds[1], rhos[1]);
+        if (include_dome)
+        {
+            init_layer(layers + 2, cathetus_z, speeds[2], rhos[2]);
+        }
+        init_layer(layers + 2 + include_dome, 1000, speeds[3], rhos[3]);
+        init_geological_model_1d(columns + x, 3 + include_dome, layers);
+    }
+    geological_model_2d model;
+    init_geological_model_2d(&model, nx, columns);
+    return model;
+}
+
+geological_model_2d new_presalt_model(unsigned int nx, float *speeds, float *rhos)
+{
+    float speeds_v[] = {1500., 2800., 4500., 5000., 4500., 2000., 3200.};
+    if (!speeds)
+    {
+        speeds = speeds_v;
+    }
+    float rhos_v[] = {1., 2., 2.16, 2.5, 2.16, 1.5, 2.6};
+    if (!rhos)
+    {
+        rhos = rhos_v;
+    }
+    geological_model_1d *columns = (geological_model_1d *)malloc(nx * sizeof(geological_model_1d));
+    float ray = nx / 6.;
+    for (unsigned int x = 0; x < nx; x++)
+    {
+        float cathetus_x = fabs(x - nx / 2.);
+        float cathetus_z;
+        unsigned int include_dome;
+        if (cathetus_x < ray)
+        {
+            cathetus_z = sqrt(ray * ray - cathetus_x * cathetus_x);
+            include_dome = 1;
+        }
+        else
+        {
+            cathetus_z = 0;
+            include_dome = 0;
+        }
+        layer *layers = (layer *)malloc((3 + 4 * include_dome) * sizeof(layer));
+        init_layer(layers, 1000, speeds[0], rhos[0]);
+        init_layer(layers + 1, 1000 - 7 * cathetus_z, speeds[1], rhos[1]);
+        if (include_dome)
+        {
+            init_layer(layers + 2, 2 * cathetus_z, speeds[2], rhos[2]);
+            init_layer(layers + 3, 2 * cathetus_z, speeds[3], rhos[3]);
+            init_layer(layers + 4, 2 * cathetus_z, speeds[4], rhos[4]);
+            init_layer(layers + 5, cathetus_z, speeds[5], rhos[5]);
+        }
+        init_layer(layers + 2 + 4 * include_dome, 1000, speeds[6], rhos[6]);
+        init_geological_model_1d(columns + x, 3 + 4 * include_dome, layers);
+    }
+    geological_model_2d model;
+    init_geological_model_2d(&model, nx, columns);
+    return model;
+}
+
 typedef struct record
 {
     float time;
@@ -218,10 +345,14 @@ registry *join_registries(registry *a, registry *b)
         return b;
     if (!b)
         return a;
-    a->number_records += b->number_records;
+    registry *c = (registry *)malloc(sizeof(registry));
+    c->number_records = a->number_records + b->number_records;
+    c->first = b->first;
     b->last->next = a->first;
-    a->first = b->first;
-    return a;
+    c->last = a->last;
+    free(a);
+    free(b);
+    return c;
 }
 
 typedef enum
@@ -248,7 +379,7 @@ registry *pulse(geological_model_1d *model,
     {
         return_time = base_time + model->times[i];
     }
-    if ((return_time < max_time) && (signal > MIN_SIGNAL))
+    if ((return_time < max_time) && (fabs(signal) > MIN_SIGNAL))
     {
         if ((way == DOWNWARD) && (i < model->n_layers))
         {
@@ -287,11 +418,7 @@ registry *pulse(geological_model_1d *model,
         }
     }
     ret = join_registries(ret, downward);
-    if ((downward) && (downward != ret))
-        free(downward);
     ret = join_registries(ret, upward);
-    if ((upward) && (upward != ret))
-        free(upward);
     return ret;
 }
 
@@ -308,7 +435,7 @@ float *measure_signals(geological_model_1d *model,
         signal = m->first;
     while (signal)
     {
-        unsigned int bin = floor(signal->time * bins_per_time);
+        unsigned int bin = (signal->time * bins_per_time);
         signals[bin] += signal->amplitude;
         signal = signal->next;
     }
@@ -324,16 +451,43 @@ float *acquire_seismic_image(geological_model_2d *model,
     {
         float *column_measurement = measure_signals(model->columns + x, max_time,
                                                     remove_multiples, nz);
-        memcpy(seismic_image + (x * nz), column_measurement, nz);
+        memcpy(seismic_image + (x * nz), column_measurement, nz * sizeof(float));
         free(column_measurement);
     }
     return seismic_image;
 }
 
+float *rasterize_geology_spd(geological_model_2d *model, unsigned int nz)
+{
+    float *image = (float *)malloc(model->nx * nz * sizeof(float));
+    float z_ratio = nz / model->depth;
+    for (unsigned int x = 0; x < model->nx; x++)
+    {
+        float current_z = 0.;
+        for (unsigned int l = 0; l < model->columns[x].n_layers; l++)
+        {
+            float height = model->columns[x].layers[l].depth * z_ratio;
+            for (unsigned int z = current_z; z < current_z + height; z++)
+            {
+                image[x * nz + z] = model->columns[x].layers[l].speed;
+            }
+            current_z += height;
+        }
+    }
+    return image;
+}
+
 int main(int argc, const char *argv[])
 {
     srand((unsigned int)time(NULL));
-    geological_model_2d model = new_senoidal_model(512, 10000, 500., 1000., 1500., 4500., 1., 2.5);
+    geological_model_2d model = new_senoidal_model(512, 5000, 500., 1000., 1500., 4500., 1., 2.5);
+    // geological_model_2d model = new_parallel_model(512, 0, NULL, NULL, NULL);
+    // geological_model_2d model = new_dome_model(512, NULL, NULL);
+    // geological_model_2d model = new_presalt_model(512, NULL, NULL);
+
+    //    printf("\n", model.columns[0]);
+    registry *m = pulse(model.columns, 0, 0., 1., DOWNWARD, 3., 0);
+
     float *seismic_image = acquire_seismic_image(&model, 3., 0, 256);
     FILE *file = fopen("seismic_image.bin", "wb");
     if (file == NULL)
@@ -347,5 +501,23 @@ int main(int argc, const char *argv[])
         printf("Error writing to file!");
         return 1;
     }
+    free(seismic_image);
+    fclose(file);
+
+    float *geology = rasterize_geology_spd(&model, 256);
+    file = fopen("geological_image.bin", "wb");
+    if (file == NULL)
+    {
+        printf("Error opening file!");
+        return 1;
+    }
+    count = fwrite(geology, sizeof(float), 512 * 256, file);
+    if (count < 512 * 256)
+    {
+        printf("Error writing to file!");
+        return 1;
+    }
+    free(geology);
+    fclose(file);
     return 0;
 }
